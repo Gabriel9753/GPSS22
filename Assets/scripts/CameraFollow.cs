2282bf8077811bedcb0a6e5e70d25d8a83a59248
Gabriel9753 <scga1011@h-ka.de>
2022-03-31 02:49:47 +0200

    better roll

--- Assets/scripts/CameraFollow.cs
+++ Assets/scripts/CameraFollow.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,9 +10,26 @@ public class CameraFollow : MonoBehaviour
     public Vector3 offset;
  
     private Vector3 velocity = Vector3.zero;
- 
+    
+    public float smoothSpeed = 2f;
+
+    public float currentZoom = 1f;
+    public float maxZoom = 3f;
+    public float minZoom = .3f;
+    public float zoomSensitivity = .7f;
+
+    float zoomSmoothV;
+    float targetZoom;
+
+
+    private void Start(){
+        targetZoom = currentZoom;
+    }
+
     void Update () {
         Vector3 goalPos = target.position + offset;
         transform.position = Vector3.SmoothDamp (transform.position, goalPos, ref velocity, smoothTime);
+
     }
+
 }
--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -14,44 +14,54 @@ public class PlayerMovement : MonoBehaviour
     public int maxDistance = 70;
     private Vector3 destination;
     public LayerMask moveMask;
-    private static GameObject player;
+    
+    public float rollSpeed = 13.0f;
+    public float walkSpeed = 8.0f;
 
     // Called when a script is enabled
-    void Start()
+    void Awake()
     {
         animator = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
-        player = gameObject;
     }
 
     // Called once every frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (!animator.GetCurrentAnimatorStateInfo(0).IsName("Rolling"))
         {
-            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray, out hit, maxDistance, moveMask))
+            if (Input.GetMouseButton(0))
             {
-                agent.speed = 9;
-                animator.SetBool("isRolling", false);
-                animator.SetBool("isRunning", true);
+                Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+                if (Physics.Raycast(ray, out hit, maxDistance, moveMask))
+                {
+                    agent.speed = walkSpeed;
+                    animator.SetBool("isRolling", false);
+                    animator.SetBool("isRunning", true);
+                    agent.SetDestination(hit.point);
+                    destination = agent.destination;
+                }
+            }
+            //Rolling
+            if (Input.GetKeyDown("space"))
+            {
+                float alpha = (float)((transform.rotation.eulerAngles.y % 360) * Math.PI)/180;
+                Vector3 forward = new Vector3((float)Math.Sin(alpha), 0, (float)Math.Cos(alpha));
+                Vector3 newDestination = transform.position + forward * (rollSpeed+3.1f);
+                agent.SetDestination(newDestination);
                 destination = agent.destination;
-                agent.SetDestination(hit.point);
+                agent.speed = rollSpeed;
+                animator.Play("Rolling");
+                animator.SetBool("isRolling", true);
+            }
+            if (Vector3.Distance(destination, transform.position) < 0.1f)
+            {
+                agent.speed = walkSpeed;
+                animator.SetBool("isRunning", false);
             }
         }
 
-        if (Input.GetKeyDown("space"))
-        {
-            animator.Play("Rolling");
-            animator.SetBool("isRolling", true);
-            agent.speed = 5;
-        }
-
-        if (Vector3.Distance(destination, transform.position) < 0.1f)
-        {
-            animator.SetBool("isRunning", false);
-        }
     }
 
     public static void Warp(Vector3 newPosition)
